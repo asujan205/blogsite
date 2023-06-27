76e1e46919bd8279385146757c355eaea50297e9
asujan205 <asujan205@gmail.com>
2023-06-27 13:29:19 +0545

    #curdisDonelets

--- src/pages/dashboard/post/index.tsx
+++ src/pages/dashboard/post/index.tsx
@@ -5,22 +5,39 @@ import { api } from "~/utils/api";
 const ViewPost = () => {
   const router = useRouter();
   const { data: post, isLoading } = api.post.all.useQuery();
+  const [title, setTitle] = React.useState("");
+  const [content, setContent] = React.useState("");
+  const [openEdit, setOpenEdit] = React.useState(false);
+  const [id, setId] = React.useState("");
 
-  const { mutate, isLoading: isDeleting } = api.post.delete.useMutation({
-    onSuccess: () => {
-      router.replace("/dashboard");
-    },
-    onError: (error) => {
-      console.log(error);
-    },
-  });
+  const { mutate: deletePost, isLoading: isDeleting } =
+    api.post.delete.useMutation({
+      onSuccess: () => {
+        router.replace("/dashboard");
+      },
+      onError: (error) => {
+        console.log(error);
+      },
+    });
+
+  const { mutate: updatePost, isLoading: isUpdating } =
+    api.post.update.useMutation({
+      onSuccess: () => {
+        router.replace("/dashboard");
+      },
+      onError: (error) => {
+        console.log(error);
+      },
+    });
 
   const handleEdit = (id: string) => {
+    setId(id);
     // mutate(id);
+    setOpenEdit(true);
   };
 
   const handleDelete = (id: any) => {
-    mutate({ id });
+    deletePost({ id });
   };
 
   if (isLoading) {
@@ -57,6 +74,38 @@ const ViewPost = () => {
           </div>
         </div>
       </div>
+      {openEdit && (
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h1 className="text-2xl font-bold">Edit Post</h1>
+          <form className="flex flex-col gap-4">
+            <input
+              type="text"
+              placeholder="Title"
+              className="rounded-md border border-gray-300 px-4 py-2"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <textarea
+              placeholder="Content"
+              className="rounded-md border border-gray-300 px-4 py-2"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+            <button
+              className="rounded-md bg-indigo-500 px-4 py-2 text-white"
+              onClick={() => updatePost({ id: id, title, content })}
+            >
+              Update
+            </button>
+            <button
+              className="rounded-md bg-red-500 px-4 py-2 text-white"
+              onClick={() => setOpenEdit(false)}
+            >
+              Close
+            </button>
+          </form>
+        </div>
+      )}
     </>
   );
 };
