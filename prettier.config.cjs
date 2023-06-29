d2ef55360868f79720b79a73d93ffe8af2ef9593
asujan205 <asujan205@gmail.com>
2023-06-29 19:47:52 +0545

    #pretier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 100,
 };
 
 module.exports = config;
